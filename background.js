3b7e4b14de0797932fa964ca8a047f961edbf4b4
Spencer Bailey <SpencerBaile@gmail.com>
2019-01-21 23:01:20 -0600

    Initial commit

---
+++ background.js
@@ -0,0 +1,13 @@
+var host = "https://smile.amazon.com/";
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+         return {redirectUrl: host + details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]};
+    },
+    {
+        urls: [
+            "*://www.amazon.com/*"
+        ],
+        types: ["main_frame", "sub_frame", "stylesheet", "script", "image", "object", "xmlhttprequest", "other"]
+    },
+    ["blocking"]
+);
